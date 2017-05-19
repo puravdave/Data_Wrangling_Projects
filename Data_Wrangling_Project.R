a86f6738aad2f43202650f57f88b82bcd98e2c49
puravdave <purav.dave@gmail.com>
2017-05-18 20:10:57 -0700

    Named file of previous code

---
+++ Data_Wrangling_Project.R
@@ -0,0 +1,22 @@
+# Load tidyverse
+
+library(tidyverse)
+
+# Load refine_original 
+
+refine_df <- read_csv("refine_original.csv")
+
+#1. clean up names - I don't know how to do that
+
+#2. Separate product code and number
+
+refine_df_2 <- refine_df %>% 
+  separate(`Product code / number`, c("product_code", "product_number"), sep = "-")
+
+#3. Add product categories
+
+#4. Add full address for geocoding
+
+refine_df_3 <- refine_df_2 %>% 
+  mutate(full_address = unite(address, city, country, sep = ", "))
+
